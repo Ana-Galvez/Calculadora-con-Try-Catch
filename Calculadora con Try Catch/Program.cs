58cc5c24859c4040e80eb8d6f585460c934f3636
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-04 20:59:08 -0300

    feat: validaciones y fixes

--- Calculadora con Try Catch/Program.cs
+++ Calculadora con Try Catch/Program.cs
@@ -7,43 +7,48 @@ namespace Calculadora_con_Try_Catch
     {
         static void Main(string[] args)
         {
-            try
+            string salir;
+            do
             {
-                Console.WriteLine("Ingrese el primer número");
-                double num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Ingrese el segundo número");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-               
-                Console.WriteLine("Ingrese el signo matemático: + - * /");
-                string signo = Console.ReadLine();
-
-                switch (signo)
+                try
                 {
-                    case "+":
-                        Console.WriteLine($"El resultado es: {num1 + num2}");
-                        break;
-                    case "-":
-                        Console.WriteLine($"El resultado es: {num1 - num2}");
-                        break;
-                    case "*":
-                        Console.WriteLine($"El resultado es: {num1 * num2}");
-                        break;
-                    case "/":
-                        if (num2 == 0) Console.WriteLine("No se puede dividir por 0");
-                        else Console.WriteLine($"El resultado es: {num1 / num2}");
-                        break;
-                    default:
-                        Console.WriteLine("Signo inválido");
-                        break;
-                }
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            
-        }
+                    Console.WriteLine("Ingrese el primer número");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
 
+                    Console.WriteLine("Ingrese el segundo número");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el signo matemático: + - * /");
+                    string signo = Console.ReadLine();
+
+                    switch (signo)
+                    {
+                        case "+":
+                            Console.WriteLine($"El resultado es: {num1 + num2}");
+                            break;
+                        case "-":
+                            Console.WriteLine($"El resultado es: {num1 - num2}");
+                            break;
+                        case "*":
+                            Console.WriteLine($"El resultado es: {num1 * num2}");
+                            break;
+                        case "/":
+                            if (num2 == 0) Console.WriteLine("No se puede dividir por 0");
+                            else Console.WriteLine($"El resultado es: {num1 / num2}");
+                            break;
+                        default:
+                            Console.WriteLine("Signo inválido");
+                            break;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("¿Desea hacer otra cuenta? si/no");
+                salir=Console.ReadLine().ToLower();
+            } while (salir == "si");
+            Console.WriteLine("Muchas gracias por usar la calculadora");
+        }
     }
 }
